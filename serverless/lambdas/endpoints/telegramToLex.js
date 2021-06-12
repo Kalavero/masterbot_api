4791647ae2de001d6e67047a3c946e1c97929016
Andre Verissimo <andrelucas.verissimo@yahoo.com>
2021-06-11 21:55:31 -0300

    Initial commit

---
+++ serverless/lambdas/endpoints/telegramToLex.js
@@ -0,0 +1,48 @@
+const AWS = require('aws-sdk');
+const Responses = require('../common/API_Responses');
+const Axios = require('axios');
+
+const lexruntime = new AWS.LexRuntime();
+
+exports.handler = async (event) => {
+  try {
+    const body = JSON.parse(event.body);
+    console.log('body: ', body);
+    const messageForLex = mapTelegramToLex(body);
+    const lexResponse = await lexruntime.postText(messageForLex).promise();
+    const messageForTelegram = mapLexToTelegram(lexResponse, body);
+
+    await sendToTelegram(messageForTelegram);
+
+    return Responses._200();
+  } catch (error) {
+    console.log('Error: ', error)
+    return Responses._400();
+  }
+}
+
+const mapTelegramToLex = body => {
+  const chatID = String(body.message.chat.id);
+  const message = body.message.text;
+  return {
+    inputText: message,
+    userId: chatID,
+    botName: 'MasterBot',
+    botAlias: 'dev',
+    sessionAttributes: {}
+  };
+}
+
+const mapLexToTelegram = (lexResponse, body) => {
+  return {
+    text: lexResponse.message,
+    chat_id: body.message.chat.id
+  };
+}
+
+const sendToTelegram = message => {
+  const token = '1793575669:AAHkbfm0H8GFbXNmw2prd-CBmt7Wd-JDlU8';
+  const telegramURL = `https://api.telegram.org/bot${token}/sendMessage`;
+
+  return Axios.post(telegramURL, message);
+}
